c06139ebf74a3cd20aca36f7662bebc2c6bf549c
Maczeny <nyantakyi012@gmail.com>
2023-03-16 11:20:02 -0700

    Task*. UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Program of UNIX being a simple operating system
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10i-19\r", stdout);
+return (1);
+}
