c9cde164b5f246c0f92fca1b38081bd98d2fccc8
Maczeny <nyantakyi012@gmail.com>
2023-03-22 15:32:58 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,36 @@
 #include "main.h"
 
 /**
- * jack_bauer - Paints the minutes of a day
- *
- * Return: no return
+ * times_table-prints the 9 times table, starting with 0
  */
-void jack_bauer(void)
+void times_table(void)
 {
+	int i, j, k;
 
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0'0;
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
