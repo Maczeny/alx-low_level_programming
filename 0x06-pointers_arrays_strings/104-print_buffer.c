20fd62f0ef5705f91f1acb87490149f1c2f281f6
Maczeny <nyantakyi012@gmail.com>
2023-03-30 14:35:02 -0700

    Two last task

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -22,27 +22,27 @@ void print_buffer(char *b, int size)
 	while (o < size)
 	{
 		j = size - o < 10 ? size - o : 10;
-		printf("%08x:", o);
+		printf("%08x: ", o);
 		for (i = 0; i < 10; i++)
 		{
-			for (i < j)
+			if (i < j)
 				printf("%02x", *(b + o + i));
 			else
-				printf(" ");
+				printf("  ");
 			if (i % 2)
 			{
 				printf(" ");
 			}
-			for (i = 0; i < j; i++)
-			{
-				int c = *(b + o + i);
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
 
-				if (c < 32 || c > 132)
-				{
-					c = '.';
-				}
-				printf("%c", c)
+			if (c < 32 || c > 132)
+			{
+				c = '.';
 			}
+			printf("%c", c);
 		}
 		printf("\n");
 		o += 10;
